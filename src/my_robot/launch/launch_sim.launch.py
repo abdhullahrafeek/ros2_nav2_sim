f5e6aa32350cfdb02a50464d7cff71f6bb4fbc1a
Abdhullah Rafeek <rafeekabdhullah13.work@gmail.com>
2026-01-30 11:08:49 +0800

    Moved map files to map directory

--- src/my_robot/launch/launch_sim.launch.py
+++ src/my_robot/launch/launch_sim.launch.py
@@ -17,6 +17,11 @@ from launch.event_handlers import OnProcessExit
 def generate_launch_description():
     package_name='my_robot'
     
+    map_path = os.path.join(
+        get_package_share_directory(package_name),
+        'maps'
+    )
+    
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -75,8 +80,8 @@ def generate_launch_description():
         executable='map_server',
         output='screen',
         parameters=[
-            {'yaml_filename':'my_new_map_save.yaml'},
-            {'use_sim_time':True}
+            {'yaml_filename': os.path.join(map_path,'my_new_map_save.yaml')},
+            {'use_sim_time': True}
         ]
     )
     
